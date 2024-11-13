e51e3ff3b7c7932152d26c3b77e450cf1b2009db
Ashish Bajpai <ashishvirgo12@gmail.com>
2024-11-13 10:03:00 +0530

    react app using CDN

---
+++ JS/react-cdn.js
@@ -0,0 +1,10 @@
+const parent=document.getElementById("root");
+const element=React.createElement("h1",{},"This is new heading");
+const ele1=React.createElement("h2",{},"Shopping Cart");
+const item1=React.createElement("li",{},"Item-1");
+const item2=React.createElement("li",{},"Item-2");
+const list=React.createElement("ul",{},item1,item2);
+const para=<p>This is new paragraph</p>;
+const root=ReactDOM.createRoot(parent);
+    //   root.render(element);
+      root.render([element,ele1,list,para]);
