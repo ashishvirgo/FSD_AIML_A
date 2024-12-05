868e9a5571e0fffca2c57b8f917e49fda2cf9644
Ashish Bajpai <ashish.bajpai@abes.ac.in>
2024-12-05 15:09:30 +0530

    stopwatch code updated

--- react-counter-app/src/Components/Stopwatch.jsx
+++ react-counter-app/src/Components/Stopwatch.jsx
@@ -1,18 +1,47 @@
 import React from 'react'
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 const Stopwatch = () => {
-    const [isRunning,setIsRunning]=useState(false)
-    function handleStartStop(){
-        setIsRunning((pre)=>!pre)
-    }
+  const [isrunning,setIsrunning]=useState(false)
+  const [time,setTime]=useState(0);
+  const [isvalid,setIsvalid]=useState(null)
+  function handleRestart(){
+      setTime(0);
+      setIsrunning(false);
+  }
+  function handleStart(){
+    setIsrunning((pre)=>!pre)
+    clearInterval(isvalid);
+  }
+  useEffect(()=>{
+     if(isrunning){
+      const id=setInterval(()=>{
+         setTime((time)=>(time+1))
+      },1000)
+      setIsvalid(id);
+     }
+     return ()=>clearInterval(isvalid)
+  },[isrunning])
+  function timeformat(time){
+          const hours=Math.floor((time/3600))
+          const hr=(hours<10)?('0'+hours):hours
+          const minutes=Math.floor((time/3600)/60);
+          const m=(minutes<10)?('0'+minutes):minutes;
+          const seconds=time%60;
+          const s=(seconds<10)?('0'+seconds):seconds;
+          return `${hr}:${m}:${s}`
+  }
   return (
     <div>
-      <h1>StopWatch App</h1>
-      <h1>Timer:</h1>
-      <button onClick={handleStartStop}>{isRunning? 'Stop':'Start'}</button>
-      <button>Reset</button>
+      <h1>
+        Stop Watch App
+      </h1>
+      <h1>{timeformat(time)}</h1>
+      <button onClick={handleStart}>{isrunning?'Stop':'Start'}</button>
+      <button onClick={handleRestart}>Reset</button>
     </div>
   )
 }
 
 export default Stopwatch
+
+
