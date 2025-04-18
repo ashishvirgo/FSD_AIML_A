47e01a9d856deb0b877e6e76a17ddc135a951020
Ashish Bajpai <ashish.bajpai@abes.ac.in>
2025-04-18 10:25:07 +0530

    add backend app

--- userapp/Server/app.js
+++ userapp/Server/app.js
@@ -1,11 +1,13 @@
 const express=require("express");
 const dotenv=require("dotenv");
+const cors=require("cors");
 const dbConnect=require("./config/db");
 const userroute=require("./Routes/userRoutes");
 dotenv.config();
 const app=express();
 const Port=process.env.PORT || 3000;
 dbConnect();
+app.use(cors);
 app.use(express.json());
 app.use("/",userroute);
 app.listen(Port,()=>{
